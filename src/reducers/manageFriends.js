9566e30fb32a66ea27620bfda68f4215e7ac7886
mark sauer-utley <mark.utley.713@gmail.com>
2018-08-24 09:37:49 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      return { ...state, friends: [...state.friends, action.friend] };
+      break;
+    case 'REMOVE_FRIEND':
+      return { ...state, friends: [...state.friends.filter(friend => friend.id !== action.id)] }
+      break;
+    default:
+      return state;
+      break;
+  }
 }
